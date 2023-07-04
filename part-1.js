3b6317d5e4b26bc32b9d6e06a370a18626a81fe9
Julio Aguilera Jr <julioaguilera337@gmail.com>
2023-07-04 01:14:48 -0500

    Done with part 1

--- part-1.js
+++ part-1.js
@@ -41,3 +41,65 @@ function createShips() {
 
 const myShips = createShips();
 console.log(`My ships ${myShips}`);
+
+//! Ask user to Enter a location to strike
+const userStrike = readlineSync
+  .question(`Bot: Enter a location to Strike (i.e "A2") `)
+  .toUpperCase();
+
+console.log(`User: You strike ${userStrike}`);
+
+const userStrikeHistory = [];
+
+function hitOrMiss(ships, strike) {
+  if (userStrikeHistory.includes(strike)) {
+    console.log(`You have already picked this location. Miss!`);
+  } else if (ships.includes(strike)) {
+    const index = ships.indexOf(strike);
+    ships.splice(index, 1);
+
+    const remainingShips = ships.length;
+    console.log(
+      `You have sunk a battleship. ${remainingShips} ship remaining.`
+    );
+  } else {
+    console.log(`You have missed!`);
+  }
+
+  userStrikeHistory.push(strike);
+}
+hitOrMiss(myShips, userStrike);
+
+function resetGame() {
+  userStrikeHistory.length = 0;
+  myShips.length = 0;
+  myShips.push(...createShips());
+  console.log("New game started. Good Luck!");
+}
+
+let gameRunning = true;
+
+function startGame() {
+  while (gameRunning) {
+    const userStrike = readlineSync
+      .question("Bot: Enter a location to Strike (i.e 'A2') ")
+      .toUpperCase();
+    console.log(`User: You struck ${userStrike}`);
+
+    hitOrMiss(myShips, userStrike);
+
+    if (myShips.every((ship) => userStrikeHistory.includes(ship))) {
+      console.log(`Congratulations! You have destroyed all battleships`);
+      const playAgain = readlineSync
+        .question("Would you like to play again (Y/N) ")
+        .toUpperCase();
+
+      if (playAgain === "Y") {
+        resetGame();
+      } else {
+        gameRunning = false;
+      }
+    }
+  }
+}
+startGame();
