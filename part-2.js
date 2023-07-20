47ed47b6d8103ccf24991941b2cdccd9aec0985c
Julio Aguilera Jr <julioaguilera337@gmail.com>
2023-07-19 22:26:57 -0500

    fixed some issues.

--- part-2.js
+++ part-2.js
@@ -1,28 +1,28 @@
-var readlineSync = require("readline-sync");
+let readlineSync = require("readline-sync");
 
 const minGridSize = 3;
 const maxGridSize = 10;
 const maxGridIndex = maxGridSize - 1;
 
-// Function to convert a number to its corresponding letter (A-J)
+//! Function to convert a number to its corresponding letter (A-J)
 const numberToLetterConverter = (number) => {
   return String.fromCharCode(65 + number);
 };
 
-// Function to create an empty grid
+//! Function to create an empty grid
 function createEmptyGrid(gridSize) {
   const grid = [];
   for (let i = 0; i < gridSize; i++) {
     const row = [];
     for (let j = 0; j < gridSize; j++) {
-      row.push({ symbol: " ~", isShip: false, isHit: false });
+      row.push({ symbol: " ~", isShip: false, isHit: false, shipName: "" });
     }
     grid.push(row);
   }
   return grid;
 }
 
-// Function to randomly place ships on the grid
+//! Function to randomly place ships on the grid
 function placeShips(grid, shipTypes) {
   for (const ship of shipTypes) {
     let isShipPlaced = false;
@@ -35,10 +35,16 @@ function placeShips(grid, shipTypes) {
         if (orientation === "horizontal") {
           for (let i = 0; i < ship.length; i++) {
             grid[row][col + i].isShip = true;
+            grid[row][col + i].shipName = ship.name;
+            grid[row][col + i].xCoord = col + i; // Store actual x-coordinate
+            grid[row][col + i].yCoord = row; // Store actual y-coordinate
           }
         } else {
           for (let i = 0; i < ship.length; i++) {
             grid[row + i][col].isShip = true;
+            grid[row + i][col].shipName = ship.name;
+            grid[row + i][col].xCoord = col; // Store actual x-coordinate
+            grid[row + i][col].yCoord = row + i; // Store actual y-coordinate
           }
         }
         isShipPlaced = true;
@@ -47,7 +53,7 @@ function placeShips(grid, shipTypes) {
   }
 }
 
-// Function to check if a ship can be placed in a specific location
+//! Function to check if a ship can be placed in a specific location
 function canPlaceShip(grid, row, col, length, orientation) {
   if (orientation === "horizontal") {
     if (col + length > grid.length) {
@@ -71,28 +77,28 @@ function canPlaceShip(grid, row, col, length, orientation) {
   return true;
 }
 
-// Function to display the grid
+//! Function to display the grid
 function displayGrid(grid) {
   const horizontalLine = "" + "".repeat(grid.length * 3);
   const verticalLine = " |";
   const header = [" "];
 
-  // Create header for the top row (numbers 1-10)
+  //! Create header for the top row (numbers 1-10)
   for (let i = 0; i < grid.length; i++) {
     header.push(`${i + 1}`.padStart(3, " "));
   }
   console.log(header.join(" "));
 
-  // Create the grid rows
+  //! Create the grid rows
   for (let i = 0; i < grid.length; i++) {
     const row = [numberToLetterConverter(i)]; // Add letter header for each row
     for (let j = 0; j < grid.length; j++) {
       const cell = grid[i][j];
       if (cell.isHit) {
         if (cell.isShip) {
-          row.push(" X"); // Mark hit ship with 'X'
+          row.push(` ${cell.shipName.charAt(0)}`); // Show the first letter of the ship name
         } else {
-          row.push(" O"); // Mark empty cell hit with 'O'
+          row.push(" *"); // Mark empty cell hit with 'O'
         }
       } else if (cell.isShip) {
         row.push(" ~"); // Display the symbol for ships that haven't been hit
@@ -103,9 +109,60 @@ function displayGrid(grid) {
     console.log(row.join(verticalLine));
     console.log(horizontalLine);
   }
+
+  // Display ships' coordinates
+  console.log("\n=== SHIPS' COORDINATES ===");
+  for (const row of grid) {
+    const shipCoordinates = row
+      .filter((cell) => cell.isShip)
+      .map(
+        (cell) => `${numberToLetterConverter(cell.yCoord)}${cell.xCoord + 1}`
+      );
+    if (shipCoordinates.length > 0) {
+      console.log(`${shipCoordinates.join(", ")}`);
+    }
+  }
 }
 
-// Function to play the game
+//!Adj. Ships
+function adjustShipTypes(gridSize) {
+  // The maximum number of ships that can fit on the grid based on its size
+  const maxTotalLength = gridSize * gridSize;
+
+  // Define your ship types with their initial lengths
+  const initialShipTypes = [
+    { name: "Carrier", length: 5 },
+    { name: "Battleship", length: 4 },
+    { name: "Cruiser", length: 3 },
+    { name: "Submarine", length: 3 },
+    { name: "Destroyer", length: 2 },
+  ];
+
+  // Calculate the total length of all ships based on their initial lengths
+  const totalInitialLength = initialShipTypes.reduce(
+    (total, ship) => total + ship.length,
+    0
+  );
+
+  // If the total length of ships is greater than the maximum allowed, adjust ship lengths
+  if (totalInitialLength > maxTotalLength) {
+    // Calculate the factor by which we need to scale down the ship lengths
+    const scalingFactor = maxTotalLength / totalInitialLength;
+
+    // Adjust the lengths of each ship based on the scaling factor
+    const adjustedShipTypes = initialShipTypes.map((ship) => ({
+      ...ship,
+      length: Math.floor(ship.length * scalingFactor),
+    }));
+
+    return adjustedShipTypes;
+  }
+
+  // If the total length is within the limit, return the initial ship types as is
+  return initialShipTypes;
+}
+
+//! Function to play the game
 function playGame() {
   let gridSize = 0;
   while (gridSize < minGridSize || gridSize > maxGridSize || isNaN(gridSize)) {
@@ -118,10 +175,11 @@ function playGame() {
     }
   }
 
+  //*shipsss
   const shipTypes = [
     { name: "Carrier", length: gridSize },
     { name: "Battleship", length: gridSize - 1 },
-    { name: "Cruiser", length: gridSize - 2 },
+    { name: "cruiser", length: gridSize - 2 },
     { name: "Submarine", length: gridSize - 2 },
     { name: "Destroyer", length: gridSize - 3 },
   ];
@@ -132,7 +190,7 @@ function playGame() {
   let totalHits = 0;
 
   while (remainingShips > 0) {
-    console.log("\n=== BATTLESHIP GAME ===");
+    console.log("\n=== BATTLESHIP GAME ===\n");
     displayGrid(grid);
     let guessRow = -1;
     while (guessRow < 0 || guessRow >= gridSize || isNaN(guessRow)) {
@@ -169,16 +227,16 @@ function playGame() {
       } else {
         cell.isHit = true;
         if (cell.isShip) {
-          console.log("It's a hit!");
+          console.log("\nIt's a hit! 💣 \n");
           totalHits++;
           if (
             totalHits === shipTypes.reduce((sum, ship) => sum + ship.length, 0)
           ) {
-            console.log("Congratulations! You sunk all the ships!");
+            console.log("\nCongratulations! You sunk all the ships! 🥇\n");
             break;
           }
         } else {
-          console.log("It's a miss!");
+          console.log("\nIt's a miss! ❌ \n");
         }
       }
     } else {
@@ -190,10 +248,10 @@ function playGame() {
     }
   }
 
-  // Display the final grid after the game ends
+  //! Display the final grid after the game ends
   console.log("\n=== FINAL GRID ===\n");
   displayGrid(grid);
 }
 
-// Start the game
+//! Start the game
 playGame();
